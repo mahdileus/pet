8e6787e1b1502296e91edf25ab728e1e812d3744
mahdi <mahdinouri1030@gmail.com>
2025-12-16 11:01:39 +0330

    master

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Footer from "../components/module/footer/Footer";
 import Navbar from "../components/module/navbar/Navbar";
 import ShopArchive from "../components/module/shop/ShopArchive";
@@ -5,13 +6,15 @@ import ShopArchive from "../components/module/shop/ShopArchive";
 
 
 
-export default  function Products() {
+export default function Products() {
 
   return (
     <div>
-        <Navbar/>
-        <ShopArchive/>
-        <Footer/>
+      <Navbar />
+      <Suspense>
+        <ShopArchive />
+      </Suspense>
+      <Footer />
 
     </div>
   );
